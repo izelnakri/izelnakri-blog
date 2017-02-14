0f2a9303f3bd0ff7ec64259a408df8b995c3456a
Izel Nakri <contact@izelnakri.com>
2017-02-14 22:42:53 +0100

    seeds removed, comment tests added

--- backend/test/models/comment_test.exs
+++ backend/test/models/comment_test.exs
@@ -3,8 +3,10 @@ defmodule Backend.CommentTest do
 
   alias Backend.Comment
 
-  @valid_attrs %{content: "some content", blog_post_id: nil}
+  @valid_attrs %{content: "some content"}
   @invalid_attrs %{content: ""}
+
+  # test for comment Comment.user_changeset works with user, shouldnt work without user
 
   test "changeset with valid attributes" do
     blog_post = insert_blog_post()
@@ -22,6 +24,21 @@ defmodule Backend.CommentTest do
 
   test "changeset with invalid attributes" do
     changeset = Comment.changeset(%Comment{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+
+  test "Comment.user_changeset with valid attributes assign confirmed_at" do
+    blog_post = insert_blog_post()
+    comment_struct = %Comment{blog_post_id: blog_post.id} |> Repo.preload(:blog_post)
+    changeset = Comment.user_changeset(comment_struct, @valid_attrs)
+    assert changeset.valid?
+    assert changeset.changes.confirmed_at != nil
+  end
+
+  test "Comment.user_changeset with invalid attributes" do
+    blog_post = insert_blog_post()
+    comment_struct = %Comment{blog_post_id: blog_post.id} |> Repo.preload(:blog_post)
+    changeset = Comment.user_changeset(comment_struct, @invalid_attrs)
     refute changeset.valid?
   end
 end
