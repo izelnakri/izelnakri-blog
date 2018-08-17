862bb4f5e8367b1d015b5f7da666e0926d1dab35
Izel Nakri <contact@izelnakri.com>
2018-08-17 22:56:09 +0200

    setup-database.sh waits for db trial

--- backend/.iex.exs
+++ backend/.iex.exs
@@ -1,2 +1,2 @@
-import Ecto.Query
+# import Ecto.Query
 alias Backend.{BlogPost, Comment, User, Tag, BlogPostTag}
--- scripts/setup-database.sh
+++ scripts/setup-database.sh
@@ -2,22 +2,38 @@
 set -e
 
 cd backend
+
+# Prepare Dialyzer if the project has Dialyxer set up
+if mix help dialyzer >/dev/null 2>&1
+then
+  echo "\nFound Dialyxer: Setting up PLT..."
+  mix do deps.compile, dialyzer --plt
+else
+  echo "\nNo Dialyxer config: Skipping setup..."
+fi
+
+# Wait for Postgres to become available.
+until psql -h $PGHOST -U "$PGUSER" -c '\q' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+echo "\nPostgres is available: continuing with database setup..."
+
 mix ecto.create
 mix ecto.migrate
 
+echo "\nPostgres migrations finished..."
 
 
 
-# #!/bin/sh
-# # Adapted from Alex Kleissner's post, Running a Phoenix 1.3 project with docker-compose
-# # https://medium.com/@hex337/running-a-phoenix-1-3-project-with-docker-compose-d82ab55e43cf
-#
+# # #!/bin/sh
+# # # Adapted from Alex Kleissner's post, Running a Phoenix 1.3 project with docker-compose
+# # # https://medium.com/@hex337/running-a-phoenix-1-3-project-with-docker-compose-d82ab55e43cf
+# #
 # set -e
 #
-# # Ensure the app's dependencies are installed
-# mix deps.get
-#
-# # Prepare Dialyzer if the project has Dialyxer set up
+# # # Prepare Dialyzer if the project has Dialyxer set up
 # if mix help dialyzer >/dev/null 2>&1
 # then
 #   echo "\nFound Dialyxer: Setting up PLT..."
