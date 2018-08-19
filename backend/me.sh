158f7a024cc791c1565411dfd817216e9b3d1dc4
Izel Nakri <contact@izelnakri.com>
2018-08-19 23:44:48 +0200

    testing super cache

--- backend/me.sh
+++ backend/me.sh
@@ -1,1 +1,1 @@
- http http://localhost:4000/me "Authorization : Bearer E4fDmaIHKVyM_X1wDyWfdnOTJSTihVDQBv_LZeQBivNa9jj1e7QTRuHpiYjNpjon"
+http http://localhost:4000/me "Authorization : Bearer E4fDmaIHKVyM_X1wDyWfdnOTJSTihVDQBv_LZeQBivNa9jj1e7QTRuHpiYjNpjon"
