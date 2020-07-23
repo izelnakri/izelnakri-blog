a33d540e14037c25d34d24e752e5893da4aea34c
Izel Nakri <contact@izelnakri.com>
2020-07-23 02:28:33 +0200

    k8s upgrades

--- k8s/bases/delete-cert-manager-namespace.sh
+++ k8s/bases/delete-cert-manager-namespace.sh
@@ -1,4 +1,27 @@
- kubectl get namespace cert-manager -o json >tmp.json
+# ONLY PROVEN TO WORK VERSION:
+# kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.9.1/cert-manager.yaml
+
+# also https://github.com/helm/charts/tree/master/stable/prometheus-postgres-exporter
+
+# in order to cert-manager correctly we need to do this:
+# kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v0.14.2/cert-manager.yaml
+# kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.2/cert-manager.yaml
+
+
+
+
+
+
+
+
+stolonctl --cluster-name=kube-stolon --store-backend=kubernetes --kube-resource-kind=configmap init --yes '{ "initMode": "new", "pgParameters": {"ssl":"off","ssl_ca_file":null,"ssl_cert_file":null,"ssl_key_file":null}  }'
+psql --host izel-stolon-proxy --port 5432 postgres -U stolon -W
+
+
+
+
+# NOTE: use this script only when k8s cant remove the cert-manager correctly
+kubectl get namespace cert-manager -o json >tmp.json
 
 nvim tmp.json
 kubectl proxy
--- k8s/init.sh
+++ k8s/init.sh
@@ -3,10 +3,8 @@
 # TRY THIS BELOW WITH v0.13
 # kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.12.0/cert-manager-no-webhook.yaml
 
-
-
-kubectl create namespace cert-manager &&
-kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true --overwrite &&
+k create namespace cert-manager &&
+k label namespace cert-manager certmanager.k8s.io/disable-validation=true --overwrite &&
 k apply -f k8s/bases/cert-manager/crds.yaml &&
 k apply -f k8s/bases/cert-manager/cert-manager-v0.9.1.yaml &&
 k apply -f k8s/bases/prometheus-operator/crd-alertmanager.yaml &&
@@ -14,7 +12,11 @@ k apply -f k8s/bases/prometheus-operator/crd-podmonitor.yaml &&
 k apply -f k8s/bases/prometheus-operator/crd-prometheus.yaml &&
 k apply -f k8s/bases/prometheus-operator/crd-prometheusrules.yaml &&
 k apply -f k8s/bases/prometheus-operator/crd-servicemonitor.yaml &&
-k apply -f k8s/bases/prometheus-operator/crd-thanosrulers.yaml
+k apply -f k8s/bases/prometheus-operator/crd-thanosrulers.yaml &&
+k create namespace argo &&
+k apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/install.yaml &&
+k create namespace argocd &&
+k apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 
 # kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.9.1/cert-manager.yaml
